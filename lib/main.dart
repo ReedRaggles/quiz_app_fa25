ab917ae84b7566a2419570aec7089ae87bc4ba0d
Gary Basile <gary815b@gmail.com>
2025-09-23 12:57:20 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_fa25/quiz.dart';
+import 'package:quiz_app_fa25/start_screen.dart';
+
+void main(){
+  runApp(
+    Quiz(),
+  );
+}
+
---
+++ lib/quiz.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_fa25/questions_screen.dart';
+import 'package:quiz_app_fa25/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+
+}
+class _QuizState extends State<Quiz>{
+ Widget activeScreen = StartScreen(switchScreen);
+
+ void switchScreen(){
+  setState(
+    (){
+      activeScreen = QuestionsScreen();
+    }
+  );
+ }
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ]
+            ),
+          ),
+          child: activeScreen,
+      )),
+    );
+  }
+}
